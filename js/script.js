cba6e45f880db3f992a25d7da4155b2a47d3fae4
Fabiano Freitas <fabiano.freitas@gmail.com>
2025-05-24 19:53:20 -0300

    Site Walenna Cilios

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Menu mobile
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
